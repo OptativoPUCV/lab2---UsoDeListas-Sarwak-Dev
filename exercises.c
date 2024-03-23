871ec996ac2b0c8d5235cf3b7a6d651dda4e218e
Your Name <you@example.com>
2024-03-23 14:15:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -142,29 +142,30 @@ int parentesisBalanceados(char *cadena) {
     while (cadena[count] != '\0') {
         if (cadena[count] == ')' || cadena[count] == ']' || cadena[count] == '}') {
             if (vacio != 0) {
-                if ((cadena[count] == ')' && *(char*)top(P) != '(') ||
-                    (cadena[count] == ']' && *(char*)top(P) != '[') || 
-                    (cadena[count] == '}' && *(char*)top(P) != '{')) {
+                if ((cadena[count] == ')' && top(P) != '(') ||
+                    (cadena[count] == ']' && top(P) != '[') || 
+                    (cadena[count] == '}' && top(P) != '{')) {
                     return 0;
                 }
                 pop(P);
                 vacio--;
             } 
-            
+
             else
                 return 0;
         } 
-        
+
         else {
             dato = cadena[count];
-            push(P, &dato);
+            push(P, dato);
             vacio++;
         }
         count++;
     }
-  if (vacio != 0) {
-    return 0;
-  }
+
+    if (vacio != 0) {
+        return 0;
+    }
 
     return 1;
 }
